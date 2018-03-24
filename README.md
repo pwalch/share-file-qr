932c60655f8143f30d3644eb250cc449ebf222d6
Pierre Walch <pwalch@gmail.com>
2018-03-24 19:09:15 +0100

    Add project structure.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 qrfile
 ==============
+
+Nice QR code reader:
+* Link to Google Play Store: [link](https://play.google.com/store/apps/details?id=me.scan.android.client)
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="qrfile",
+    version="0.1",
+    description="QR code based file sharing between "
+        "a computer and a phone.",
+    author="Pierre Walch",
+    packages=["qrfile"],
+)
