15f9cec839421f189a1adc64f497bd2aba785a25
Pierre Walch <code@pwal.ch>
2018-03-25 20:55:30 +0200

    Add terminal QR code delivery.

--- qrfile/qrcodes.py
+++ qrfile/qrcodes.py
@@ -1,10 +1,19 @@
-from io import BytesIO
-import qrcode
+from io import BytesIO, StringIO
+import qrcode as qr
 from qrcode.image.svg import SvgImage
 
 
-def generate_qrcode_utf8(file_url: str) -> bytes:
-    qrcode_handle = BytesIO()
-    qrcode_svg: SvgImage = qrcode.make(file_url, image_factory=SvgImage)
-    qrcode_svg.save(qrcode_handle)
-    return qrcode_handle.getvalue()
+def generate_utf8_svg(file_url: str) -> bytes:
+    bytes_handle = BytesIO()
+    svg_image: SvgImage = qr.make(file_url, image_factory=SvgImage)
+    svg_image.save(bytes_handle)
+    return bytes_handle.getvalue()
+
+
+def generate_ascii(file_url: str) -> str:
+    qr_code = qr.QRCode()
+    qr_code.add_data(file_url)
+
+    string_handle = StringIO()
+    qr_code.print_ascii(string_handle)
+    return string_handle.getvalue()
