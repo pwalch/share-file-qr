f3a6914c124d6ae6713cd28d8089508a5ea64c70
Pierre Walch <code@pwal.ch>
2018-03-25 01:39:09 +0100

    Add error message for connection errors.

--- qrfile/network.py
+++ qrfile/network.py
@@ -1,4 +1,11 @@
 import socket
+from click import ClickException
+
+LOCALHOST_IP_ADDRESS = "127.0.0.1"
+
+
+class LocalNetworkError(ClickException):
+    pass
 
 
 def get_local_ip_address() -> str:
@@ -10,8 +17,13 @@ def get_local_ip_address() -> str:
         local_socket.connect(('10.255.255.255', 1))
         ip_address = local_socket.getsockname()[0]
     except:  # noqa: E722
-        ip_address = '127.0.0.1'
+        ip_address = LOCALHOST_IP_ADDRESS
     finally:
         local_socket.close()
+
+    if ip_address == LOCALHOST_IP_ADDRESS:
+        raise LocalNetworkError(
+            "Could find valid IP address for a local network. "
+            "Verify that you are connected to a router.")
 
     return ip_address
