b61b51e819aec95012848d85541f2eb64226f3ab
Pierre Walch <pwalch@gmail.com>
2018-03-25 20:35:27 +0200

    Standardize packaging.

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
 qrfile
-==============
-
-Nice QR code reader:
-* Link to Google Play Store: [link](https://play.google.com/store/apps/details?id=me.scan.android.client)
--- setup.py
+++ setup.py
@@ -1,12 +1,50 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+"""
+Publication
+
+Steps:
+* step into Pipenv's virtualenv
+* `python setup.py sdist`
+* `twine upload --repository-url https://test.pypi.org/legacy/ dist/*`
+"""
+
+import sys
+from setuptools import setup
+from pathlib import Path
+
+assert sys.version_info >= (3, 6, 0)
+
+
+def get_long_description():
+    readme_md = Path(__file__).parent / 'README.md'
+    with open(readme_md, encoding='utf8') as ld_file:
+        return ld_file.read()
+
 
 setup(
-    name="qrfile",
-    version="0.1",
-    description="QR code based file sharing between "
-        "a computer and a phone.",
-    author="Pierre Walch",
+    name="share-file-qr",
+    version="0.1.0",
     packages=["qrfile"],
+    python_requires=">=3.6",
+    install_requires=[
+        "click>=6.7,<7.0",
+        "qrcode>=6.0,<7.0",
+        "tornado>=5.0,<6.0"
+    ],
+    entry_points={"console_scripts": ["share-file-qr=qrfile.cli:cli"]},
+    license="GPLv3",
+    description="Share files from the terminal of your computer to a smartphone by scanning a QR code.",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    author="Pierre Walch",
+    author_email="contact@pwal.ch",
+    url="https://github.com/pwalch/share-file-qr",
+    keywords="qr code smartphone share file",
+    classifiers=[
+        "Development Status :: 3 - Alpha", "Intended Audience :: Developers",
+        "Topic :: Communications :: File Sharing",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3.6",
+    ]  # yapf: disable
 )
