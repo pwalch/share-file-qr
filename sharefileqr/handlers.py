5a0bf6fcf5fe242a81d044089cb5a46c0eb3e7e1
Pierre Walch <pwalch@gmail.com>
2021-12-11 16:14:04 +0100

    Address linting violations in sharefileqr module

--- sharefileqr/handlers.py
+++ sharefileqr/handlers.py
@@ -1,19 +1,26 @@
+from typing import Optional
+
 import tornado.web
 
 
 class ImageHandler(tornado.web.RequestHandler):
-    def initialize(self, qrcode_svg_utf8):
+    def initialize(self, qrcode_svg_utf8: bytes) -> None:
         self.qrcode_svg_utf8 = qrcode_svg_utf8
 
-    def get(self):
+    def get(self) -> None:
         self.write(self.qrcode_svg_utf8)
         self.set_header("Content-Type", "image/svg+xml; charset=utf-8")
 
 
-class MimedStaticFileHandler(tornado.web.StaticFileHandler):
-    def initialize(self, path, mime_type):
+class OctetStreamStaticFileHandler(tornado.web.StaticFileHandler):
+    def initialize(self, path: str, default_filename: Optional[str] = None) -> None:
         super().initialize(path)
-        self.mime_type = mime_type
+        # TODO: First had python-magic as a dependency, but this
+        # forced users to install the C library libmagic.
+        # To keep the application easy to install, this python-magic
+        # dependency was removed, and the mime type is always set
+        # as a blob of bytes.
+        self.mime_type = "application/octet-stream"
 
-    def set_extra_headers(self, path):
+    def set_extra_headers(self, path: str) -> None:
         self.set_header("Content-Type", self.mime_type)
